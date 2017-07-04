eeae890f83eddc58bd0092058c862d1c085b5cfe
Lei <Crescent-Saturn@users.noreply.github.com>
2017-07-04 11:35:28 -0400

    Well DONE

--- wx_auto_re.py
+++ wx_auto_re.py
@@ -5,15 +5,28 @@ from wxpy import *
 import get_gas_price
 
 
-def send_msg():
-    msg = get_gas_price.getPrice()
-    bot.self.send(msg)
-    # my_friend.send(u'这是一条自动发送测试消息，如果收到请告知我，谢谢！')
+def get_price():
+    price = get_gas_price.getPrice()
+    bot.self.send(price)
+    print(price)
 
 
 bot = Bot()
-my_friend = bot.friends().search(u'')[0]
-schedule.every(0.1).minutes.do(send_msg)
+# my_friend = bot.friends().search(u'')[0]
+
+
+@bot.register()
+def auto_reply(msg):
+    # print(u'【自动回复】(' + str(msg.create_time) +') 收到消息)')
+    if 7 <= msg.receive_time.hour < 23:
+        msg.forward(bot.self, prefix='From: {}'.format(msg.sender))
+    else:
+        return u'【自动回复】(' + str(msg.create_time) +') 收到消息：{}'.format(msg, msg.type) + u'\n不出意外的情况下我应该在睡觉，请理解！醒来后回复~'
+
+# embed()
+
+schedule.every(6).hours.do(get_price)
+print(get_gas_price.getPrice())
 
 while True:
     schedule.run_pending()
