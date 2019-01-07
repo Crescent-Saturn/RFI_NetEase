6e3439bda2238b91f1d49f6f4d5f36e39d2c8c9d
Lei <Crescent-Saturn@users.noreply.github.com>
2019-01-06 20:18:53 -0500

    new wechat happy new year

---
+++ wxpy_test.py
@@ -0,0 +1,29 @@
+from wxpy import *
+import random
+import time
+
+OkWords = []
+with open('blessings.txt', 'r') as f:
+    lines = f.readlines()
+    for item in lines:
+        item = item.strip()
+        if not len(item):
+            continue
+        OkWords.append(item)
+
+# msg = random.choice(OkWords)
+bot = Bot(cache_path=True)
+
+fr_list = bot.friends()
+
+
+# me = fr_list[0]
+# me.send(f'亲爱的{me.name}，' + msg + '\n 雷磊')
+# print(fr_list)
+
+for friend in new_list:
+    msg = random.choice(OkWords)
+    friend.send(f'祝愿{friend.name}和家人，' + msg)
+    time.sleep(.2)
+
+# print(my_friends.stats_text())
