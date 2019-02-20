a0ba586c3f8f647cc1e7a91a6c25be929ae42916
Lei <Crescent-Saturn@users.noreply.github.com>
2019-02-20 10:48:19 -0500

    Fix 404 error

--- rfi_test.py
+++ rfi_test.py
@@ -1,24 +1,52 @@
 import requests
-from datetime import datetime
+# from datetime import datetime
+import sys
 
 url_base = 'http://telechargement.rfi.fr/rfi/francais/audio/jff/'
-
+# url_spe = 'http://telechargement.rfi.fr/savoirs/apprendre/actu/jff/jff_19022019.mp3'
+url_spe = 'http://telechargement.rfi.fr/savoirs/apprendre/actu/jff/jff_'
+ 
 # url_tg = 'http://telechargement.rfi.fr/rfi/francais/audio/jff/201812/journal_francais_facile_20h00_-_20h10_tu_20181231.mp3'
-dt = datetime.now()
+# dt = datetime.now()
 # print(dt.strftime("%Y%m%d"))
 
 
-url_time_m = dt.strftime("%Y%m")
-url_time_d = dt.strftime("%Y%m%d")
+# url_time_m = dt.strftime("%Y%m")
+# url_time_d = dt.strftime("%Y%m%d")
+url_date = sys.argv[1]
+
+url_month = url_date[:-2]
 url_news = 'journal_francais_facile_20h00_-_20h10_tu_'
 
-fileName = url_news + url_time_d + '.mp3'
+# fileName = url_news + url_month + '.mp3'
+fileName = ''.join([url_news,url_date,'.mp3'])
 
-url_tg = url_base + url_time_m + '/' + fileName
+# fileName_new = url_date[-2:] + url_date[-4:-2] + url_date[:4] + '.mp3'
+fileName_new = ''.join([url_date[-2:], url_date[-4:-2], url_date[:4], '.mp3'])
 
-print(url_tg)
-print(fileName)
+# url_tg = url_base + url_time_m + '/' + fileName
+url_tg = ''.join([url_base,url_month,'/',fileName])
+
+# print(url_tg)
+# print(fileName)
+
+# saveName = ''.join([url_date,'.mp3'])
+# saveName = fileName.replace(url_news,'')
+# print(saveName)
+
 
 r = requests.get(url_tg)
-with open(fileName, 'wb') as code:
+
+if r.status_code == 404:
+    url_tg = ''.join([url_spe, fileName_new])
+    # print(url_tg)
+    r = requests.get(url_tg)
+
+
+
+# print(r.status_code)
+with open(fileName.replace(url_news,''), 'wb') as code:
     code.write(r.content)
+
+# print(f'{saveName} has been downloaded to the disk!')
+print('New mp3 has been downloaded to the disk!')
